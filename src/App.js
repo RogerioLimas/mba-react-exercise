9c284a2b33910c0c6a6d51a5de6d029dae638f44
Emilio Resende <emilio.resende@coxautoinc.com>
2021-03-23 20:48:16 -0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 
-import ImcTableView from './views/ImcTableView';
-import ImcView from './views/ImcView';
-import ImcController from './controllers/ImcController';
-import Person from './domain/Person';
+import ImcTableView from "./views/ImcTableView";
+import ImcView from "./views/ImcView";
+import ImcController from "./controllers/ImcController";
+import Person from "./domain/Person";
 
 import "./App.css";
 
 function App() {
-
-  const [controller,] = useState(new ImcController());
+  const [controller] = useState(new ImcController());
   const [person, setPerson] = useState(new Person());
-  
-  const calculateImc = async () => {
-    const heightElem = document.querySelector("#altura");
-    const weightElem = document.querySelector("#peso");
-  
-    if(!heightElem) throw Error("height is required field!");
-    if(!weightElem) throw Error("weight is required field!");
-  
+
+  const heightElem = useRef();
+  const weightElem = useRef();
+
+  const calculateImc = async (evt) => {
+    evt.preventDefault();
+
     var newPerson = new Person(
-      parseFloat(heightElem.value), 
-      parseFloat(weightElem.value));
-  
+      parseFloat(heightElem.current.value),
+      parseFloat(weightElem.current.value)
+    );
+
     const calculatedPerson = await controller.calculate(newPerson.toObject());
     setPerson(calculatedPerson);
-    
-  }
+  };
 
   return (
     <div className="container">
@@ -36,17 +34,19 @@ function App() {
             <ImcTableView />
           </div>
           <hr />
-          <div className="row">
-            <label>Altura</label>
-            <input id="altura" placeholder="0.00" />
-          </div>
-          <div className="row">
-            <label>Peso</label>
-            <input id="peso" placeholder="0.00" />
-          </div>
-          <button type="button" onClick={calculateImc} className="action">
-            Calcular
-          </button>
+          <form onSubmit={calculateImc}>
+            <div className="row">
+              <label>Altura</label>
+              <input id="altura" placeholder="0.00" ref={heightElem} />
+            </div>
+            <div className="row">
+              <label>Peso</label>
+              <input id="peso" placeholder="0.00" ref={weightElem} />
+            </div>
+            <button type="submit" className="action">
+              Calcular
+            </button>
+          </form>
         </div>
       </div>
       <hr />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'proxy-polyfill/proxy.min.js';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
